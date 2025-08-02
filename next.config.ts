30838364dace9a0e81129ef730a0cac1d6db037c
Keerththan <keerththant@gmail.com>
2025-08-02 15:41:54 +0530

    eslint removed during build again

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 // next.config.js
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
+    // ❗ This should disable ESLint during builds
     ignoreDuringBuilds: true,
   },
 };
